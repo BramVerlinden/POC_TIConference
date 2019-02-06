ee89af778774abbc522ea0e03a8c8808a683a3e5
Yannick Slegers <yannick.slegers@student.kdg.be>
2019-02-06 14:49:00 +0100

    reactive mongo template

--- src/main/java/be/kdg/pocticonference/PocticonferenceApplication.java
+++ src/main/java/be/kdg/pocticonference/PocticonferenceApplication.java
@@ -1,17 +1,27 @@
 package be.kdg.pocticonference;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@EnableReactiveMongoRepositories
-@EnableMongoAuditing
 @SpringBootApplication
 public class PocticonferenceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PocticonferenceApplication.class, args);
+    }
+
+    @Autowired
+    MongoClient mongoClient;
+
+    @Bean
+    public ReactiveMongoTemplate reactiveMongoTemplate(){
+        return new ReactiveMongoTemplate(mongoClient, "poc");
     }
 
 }
--- src/main/java/be/kdg/pocticonference/repository/FineRepository.java
+++ src/main/java/be/kdg/pocticonference/repository/FineRepository.java
@@ -3,7 +3,13 @@ package be.kdg.pocticonference.repository;
 import be.kdg.pocticonference.model.Fine;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface FineRepository extends ReactiveMongoRepository<Fine, Integer> {
+
+    Mono<Fine> findByLicensePlate(String licensePlate);
+
+    Flux<Fine> findAllByLicensePlate(String licensePlate);
 }
