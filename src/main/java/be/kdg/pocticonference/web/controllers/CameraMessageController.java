8fdd1136e2d1acd48f3b0009789f065494412be3
DennisDR <dennis.deroover@student.kdg.be>
2019-02-07 13:05:22 +0100

    webclient created

--- src/main/java/be/kdg/pocticonference/web/controllers/CameraMessageController.java
+++ src/main/java/be/kdg/pocticonference/web/controllers/CameraMessageController.java
@@ -2,18 +2,10 @@ package be.kdg.pocticonference.web.controllers;
 
 import be.kdg.pocticonference.model.CameraMessage;
 import be.kdg.pocticonference.services.api.CameraMessageService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 //controller
