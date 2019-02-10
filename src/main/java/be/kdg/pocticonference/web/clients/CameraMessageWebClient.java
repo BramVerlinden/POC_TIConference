d77180be5492613a792250b9b7ea63a698f9b84c
DennisDR <dennis.deroover@student.kdg.be>
2019-02-10 12:06:21 +0100

    update webclient

--- src/main/java/be/kdg/pocticonference/web/clients/CameraMessageWebClient.java
+++ src/main/java/be/kdg/pocticonference/web/clients/CameraMessageWebClient.java
@@ -2,15 +2,7 @@ package be.kdg.pocticonference.web.clients;
 
 import be.kdg.pocticonference.model.CameraMessage;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 public class CameraMessageWebClient {
     private WebClient client = WebClient.create("http://localhost:7890");
-
-    private Mono<CameraMessage> cameraMessageMono = client.get()
-            .uri("/CameraMessage/{id}", "1")
-            .retrieve()
-            .bodyToMono(CameraMessage.class);
-
-    public 
 }
