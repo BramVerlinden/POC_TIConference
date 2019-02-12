942ba5785932dc6a41c0e2e639f6147888006596
wannesverdonck <wannes.verdonck@student.kdg.be>
2019-02-12 16:01:28 +0100

    Fines

--- src/main/java/be/kdg/pocticonference/repository/FineRepository.java
+++ src/main/java/be/kdg/pocticonference/repository/FineRepository.java
@@ -1,6 +1,8 @@
 package be.kdg.pocticonference.repository;
 
+import be.kdg.pocticonference.model.CameraMessage;
 import be.kdg.pocticonference.model.Fine;
+
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/be/kdg/pocticonference/service/CameraMessageService.java
+++ src/main/java/be/kdg/pocticonference/service/CameraMessageService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Service
 public class CameraMessageService {
     private final CameraMessageRepository repo;
@@ -16,6 +18,7 @@ public class CameraMessageService {
 
     public Flux<CameraMessage> getAllMessages(){
         return repo.findAll();
+
     }
 
     public Mono<CameraMessage> getMessageById(String id){
